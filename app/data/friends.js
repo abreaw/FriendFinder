94792c7810420d6cf8bf270c9f82feb2a5ec6a53
Brea Torres <abreaw328@gmail.com>
2018-01-07 21:34:15 -0500

    added friend object array code

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+// friends object to store the data from the survey results
+
+var friendArray = [
+{
+	name: "My First Friend",
+	photo: "https://vignette.wikia.nocookie.net/barney/images/6/69/Image-1418515788.jpg/revision/latest?cb=20160907185103",
+	scores: [5,5,5,5,5,5,5,5,5,5]
+},
+{
+	name: "Frienemies",
+	photo: "http://images.entertainment.ie/images_content/rectangle/620x372/teen.jpg",
+	scores: [1,1,1,1,1,1,1,1,1,1]
+},
+{
+	name: "Acquaintances",
+	photo: "https://uploads.disquscdn.com/images/7a3cadeec0e8130658fe3c7329ca598b38901ee57243b1c525159a5d04b013df.png",
+	scores: [3,3,3,3,3,3,3,3,3,3]
+}];
+
+module.exports = friends; 
