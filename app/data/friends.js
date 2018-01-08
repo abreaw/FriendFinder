badfdf66f506feb01001d8412c54e60799524d57
Brea Torres <abreaw328@gmail.com>
2018-01-07 22:19:56 -0500

    home page design drafted

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,6 +16,11 @@ var friendArray = [
 	name: "Acquaintances",
 	photo: "https://uploads.disquscdn.com/images/7a3cadeec0e8130658fe3c7329ca598b38901ee57243b1c525159a5d04b013df.png",
 	scores: [3,3,3,3,3,3,3,3,3,3]
+},
+{
+	name: "Mr. Rogers",
+	photo: "https://hips.hearstapps.com/esq.h-cdn.co/assets/17/21/1600x800/landscape-1495543750-gettyimages-3408775.jpg?resize=980:*",
+	scores: [2,5,4,5,4,3,4,2,5,5]
 }];
 
 module.exports = friends; 
