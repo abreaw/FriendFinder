d5f99a386787b7aab055510473af23449a184882
Brea Torres <abreaw328@gmail.com>
2018-01-13 09:47:47 -0500

    changed route

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use(express.static(__dirname + '/public'));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
